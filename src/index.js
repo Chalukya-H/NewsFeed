e891a39e2cdd550f342f032a7777eb13555bcd67
Chalukya H <62981809+Chalukya-H@users.noreply.github.com>
2020-07-28 23:59:01 +0530

    'added-styling'

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css'; 
 import App from './App';
 import {Provider} from 'react-redux'
-import configureStore from './store/configureStore'
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import configureStore from './store/configureStore' 
 
 const store = configureStore()
 store.subscribe( () =>{
